48827a2ac5f58e4ca1ee86768662bf6f9b1259a9
Akihito KIRISAKI <kirisaki@klaraworks.net>
2018-07-06 00:09:10 +0900

    Add rectangle around text

--- src/Diagram.elm
+++ src/Diagram.elm
@@ -13,13 +13,32 @@ type Edge = Edge Node Node
 type Graph = Graph (Array Node) (Array Edge)
 
 render : Graph -> Svg.Svg msg
-render (Graph ns es) = Svg.svg [] <| Array.toList <| Array.map renderNode ns
+render (Graph ns es) = Svg.svg
+                       [ Attr.width "300"
+                       , Attr.height "300"
+                       , Attr.fontSize "10pt"
+                       ]
+                       <| Array.toList <| Array.map renderNode ns
 
 renderNode : Node -> Svg.Svg msg
 renderNode n =
-    Svg.g
-        [ Attr.width "100", Attr.height "100"]
-        [ Svg.text_
-              [ Attr.y "20" ]
+    Svg.svg
+        [ Attr.width <| flip (++) "em" <| toString <| String.length n.label
+        , Attr.height "3em"
+        ]
+        [ Svg.rect
+          [ Attr.width "100%"
+          , Attr.height "100%"
+          , Attr.fill "white"
+          , Attr.stroke "black"
+          , Attr.strokeWidth "1"
+          ]
+              []
+        , Svg.text_
+            [ Attr.x "50%"
+            , Attr.y "55%"
+            , Attr.textAnchor "middle"
+            , Attr.fontSize "1em"
+            ]
               [ Svg.text n.label ]
         ]
